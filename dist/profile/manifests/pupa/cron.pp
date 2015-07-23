0b6e6678c16d79c356c46aa69836d6b9bf9fd58a
Daniele Sluijters <daenney@users.noreply.github.com>
2015-07-23 23:22:43 +0200

    pupa/cron: Fix missing ,'s

--- dist/profile/manifests/pupa/cron.pp
+++ dist/profile/manifests/pupa/cron.pp
@@ -1,9 +1,9 @@
 class profile::pupa::cron {
   cron { 'pdeploy':
     ensure => 'present',
-    command => '/usr/local/bin/pdeploy production > /var/log/puppetlabs/puppet/pdeploy.log >2&1'
-    minutes => range(5, 55,10)
-    require => Class['::profile::pupa::binstub']
+    command => '/usr/local/bin/pdeploy production > /var/log/puppetlabs/puppet/pdeploy.log >2&1',
+    minutes => range(5, 55,10),
+    require => Class['::profile::pupa::binstub'],
   }
 
   cron { 'git pull pupa': ensure  => 'absent', }
