e982deb0e2825ac42eeedea4af7f7135ca04f15e
Daniele Sluijters <daenney@users.noreply.github.com>
2015-07-23 23:30:11 +0200

    pupa/cron: Fix stderr redirect

--- dist/profile/manifests/pupa/cron.pp
+++ dist/profile/manifests/pupa/cron.pp
@@ -1,7 +1,7 @@
 class profile::pupa::cron {
   cron { 'pdeploy':
     ensure  => 'present',
-    command => '/usr/local/bin/pdeploy production > /var/log/puppetlabs/puppet/pdeploy.log >2&1',
+    command => '/usr/local/bin/pdeploy production > /var/log/puppetlabs/puppet/pdeploy.log 2>&1',
     minute  => range(5, 55,10),
     require => Class['::profile::pupa::binstub'],
   }
