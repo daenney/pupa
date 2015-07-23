3f038efb3b5fd60f3cadba5daeeae95b894049a1
Daniele Sluijters <daenney@users.noreply.github.com>
2015-07-23 17:25:48 +0200

    Configure hiera with deep_merge behaviour
    * Install the `deep_merge` gem
    * Configure hiera's `merge_behaviour`
    
    `deep_merge` is the most practical and usually expected behaviour
    of recursively merging hashes instead of just top keys.

--- bootstrap/manifests/hiera.pp
+++ bootstrap/manifests/hiera.pp
@@ -3,6 +3,11 @@ package { 'hiera-eyaml':
   provider => 'puppet_gem',
 }
 
+package { 'deep_merge':
+  ensure   => '1.0.1',
+  provider => 'puppet_gem',
+}
+
 file { '/etc/puppetlabs/keys':
   ensure => 'directory',
   owner  => 'root',
