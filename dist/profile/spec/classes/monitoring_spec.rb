f12f7e99304c3ee8401441ef47157e2ca61b899b
Daniele Sluijters <daenney@users.noreply.github.com>
2015-12-17 11:51:23 +0100

    profile/monitoring: Actually add the spec
    This follows up on a559f03806f3fd90039d583b17ed3dc372eadea9. Though
    that commit contained all the necessary dependencies and other
    fixtures, it didn't contain the actual spec itself.

---
+++ dist/profile/spec/classes/monitoring_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'profile::monitoring' do
+  let(:pre_condition) {
+    'include apt'
+  }
+  let(:facts) { {:osfamily => 'Debian',
+                 :concat_basedir => '/tmp',
+                 :lsbdistcodename => 'trusty',
+                 :lsbdistid => 'Ubuntu' } }
+  it { is_expected.to contain_class('collectd') }
+  it { is_expected.to contain_class('facette') }
+
+  it { is_expected.to contain_class('collectd::plugin::cpu') }
+  it { is_expected.to contain_class('collectd::plugin::df') }
+  it { is_expected.to contain_class('collectd::plugin::interface') }
+  it { is_expected.to contain_class('collectd::plugin::load') }
+  it { is_expected.to contain_class('collectd::plugin::memory') }
+  it { is_expected.to contain_class('collectd::plugin::processes') }
+  it { is_expected.to contain_class('collectd::plugin::rrdtool') }
+  it { is_expected.to contain_class('collectd::plugin::users') }
+  it { is_expected.to contain_class('collectd::plugin::syslog') }
+
+  it { is_expected.to contain_collectd__plugin__ping('irc').with({
+    :hosts => ['8.8.8.8', '8.8.4.4', '2001:4860:4860::8888', '2001:4860:4860::8844'],
+    :interval => 5,
+  })}
+
+  it { is_expected.to contain_ufw__allow('allow-world-facette').with({
+    :port => 12003,
+  })}
+end
